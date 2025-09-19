721c5075e12ecf9a945f86af06e713d01e7423a6
Ghazy <dianghazy@protonmail.com>
2025-09-19 13:41:11 +0700

    tidy up

--- internal/models/dto/auth_dto.go
+++ internal/models/dto/auth_dto.go
@@ -1,6 +1,5 @@
 package dto
 
-// LoginRequest is the standardized request payload for /auth/login
 type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -9,5 +8,5 @@ type LoginRequest struct {
 type AuthTokens struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token,omitempty"`
-	ExpiresIn    int64  `json:"expires_in,omitempty"` // seconds until access token expiry
+	ExpiresIn    int64  `json:"expires_in,omitempty"`
 }
